7e60f0986923842ee6bbaa7a41910248150628fe
Cameron Stewart <cameron.stewart@jhu.edu>
2020-01-29 15:23:23 -0500

    Testing

--- code/3_simulate/1_simulate.py
+++ code/3_simulate/1_simulate.py
@@ -12,20 +12,19 @@ def run(parameters, group_name, replication):
     return True
 
 
-ray.init(num_cpus=6)
+ray.init(num_cpus=4)
 param_file = getcwd() + '/../../data/parameters/parameters.h5'
-replications = 1
+replications = 500
 
 group_names = ['msm_white_male', 'msm_black_male', 'msm_hisp_male', 'idu_white_male', 'idu_black_male',
                'idu_hisp_male', 'idu_white_female', 'idu_black_female', 'idu_hisp_female', 'het_white_male',
                'het_black_male', 'het_hisp_male', 'het_white_female', 'het_black_female', 'het_hisp_female']
 
-group_names = ['msm_black_male', 'msm_hisp_male']
-group_names = ['msm_black_male']
+#group_names = ['msm_black_male', 'msm_hisp_male']
 
 for group_name in group_names:
     print(group_name)
-    futures = [run.remote(pearl.Parameters(path=param_file, group_name=group_name, comorbidity_flag=1,
+    futures = [run.remote(pearl.Parameters(path=param_file, group_name=group_name, comorbidity_flag=0,
                                            sensitivity_analysis_list=[0, 0, 0, 0, 0, 0]), group_name, replication)
                for replication in range(replications)]
     ray.get(futures)
