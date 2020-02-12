78c7410a54faa8699bdb5aae52194ec8adaa385f
Cameron Stewart <cameron.stewart@jhu.edu>
2020-02-12 10:10:34 -0500

    Add Dx Reduce Flag

--- code/3_simulate/1_simulate.py
+++ code/3_simulate/1_simulate.py
@@ -14,7 +14,7 @@ def run(parameters, group_name, replication):
 
 ray.init(num_cpus=4)
 param_file = getcwd() + '/../../data/parameters/parameters.h5'
-replications = 100
+replications = 1
 
 group_names = ['msm_white_male', 'msm_black_male', 'msm_hisp_male', 'idu_white_male', 'idu_black_male',
                'idu_hisp_male', 'idu_white_female', 'idu_black_female', 'idu_hisp_female', 'het_white_male',
@@ -24,7 +24,7 @@ group_names = ['msm_white_male', 'msm_black_male', 'msm_hisp_male', 'idu_white_m
 
 for group_name in group_names:
     print(group_name)
-    futures = [run.remote(pearl.Parameters(path=param_file, group_name=group_name, comorbidity_flag=0,
+    futures = [run.remote(pearl.Parameters(path=param_file, group_name=group_name, comorbidity_flag=0, dx_reduce_flag=0,
                                            sensitivity_analysis_list=[0, 0, 0, 0, 0, 0]), group_name, replication)
                for replication in range(replications)]
     ray.get(futures)
--- code/3_simulate/2_process_raw.py
+++ code/3_simulate/2_process_raw.py
@@ -9,7 +9,9 @@ pd.options.display.max_rows = 6000
 # Define directories
 cwd = os.getcwd()
 h5_dir = cwd + '/../../out/raw'
-feather_dir = cwd + '/../../out/processed/reduced'
+feather_dir = cwd + '/../../out/processed/comorbid'
+
+comorbidity = True
 
 
 group_names = ['msm_white_male', 'msm_black_male', 'msm_hisp_male', 'idu_white_male', 'idu_black_male',
@@ -19,7 +21,7 @@ group_names = ['msm_white_male', 'msm_black_male', 'msm_hisp_male', 'idu_white_m
 #group_names = ['msm_white_male', 'msm_black_male', 'msm_hisp_male']
 #group_names = ['idu_white_female']
 
-replications = 100
+replications = 50
 
 for group_name in group_names:
     in_care_count = pd.DataFrame()
@@ -40,27 +42,30 @@ for group_name in group_names:
     years_out = pd.DataFrame()
     random_params = pd.DataFrame()
 
-    #smoking_count = pd.DataFrame()
-    #hcv_count = pd.DataFrame()
-    #anxiety_count = pd.DataFrame()
-    #depression_count = pd.DataFrame()
-    #ckd_count = pd.DataFrame()
-    #lipid_count = pd.DataFrame()
-    #diabetes_count = pd.DataFrame()
-    #hypertension_count = pd.DataFrame()
+    smoking_count = pd.DataFrame()
+    hcv_count = pd.DataFrame()
+    anxiety_count = pd.DataFrame()
+    depression_count = pd.DataFrame()
+    ckd_count = pd.DataFrame()
+    lipid_count = pd.DataFrame()
+    diabetes_count = pd.DataFrame()
+    hypertension_count = pd.DataFrame()
 
-    #dead = pd.DataFrame()
-    #smoking_dead = pd.DataFrame()
-    #hcv_dead = pd.DataFrame()
-    #anxiety_dead = pd.DataFrame()
-    #depression_dead = pd.DataFrame()
-    #ckd_dead = pd.DataFrame()
-    #lipid_dead = pd.DataFrame()
-    #diabetes_dead = pd.DataFrame()
-    #hypertension_dead = pd.DataFrame()
+    dead = pd.DataFrame()
+    smoking_dead = pd.DataFrame()
+    hcv_dead = pd.DataFrame()
+    anxiety_dead = pd.DataFrame()
+    depression_dead = pd.DataFrame()
+    ckd_dead = pd.DataFrame()
+    lipid_dead = pd.DataFrame()
+    diabetes_dead = pd.DataFrame()
+    hypertension_dead = pd.DataFrame()
 
-    #greater_than_zero_count = pd.DataFrame()
-    #greater_than_one_count = pd.DataFrame()
+    comorbid_zero_count = pd.DataFrame()
+    comorbid_one_count = pd.DataFrame()
+    comorbid_two_count = pd.DataFrame()
+    comorbid_three_count = pd.DataFrame()
+    comorbid_four_count = pd.DataFrame()
 
     print(group_name)
     for replication in range(replications):
@@ -92,29 +97,32 @@ for group_name in group_names:
 
             random_params = random_params.append(store['random_params'])
 
-            """
-            smoking_count = smoking_count.append(store['smoking_count'])
-            hcv_count = hcv_count.append(store['hcv_count'])
-            anxiety_count = anxiety_count.append(store['anxiety_count'])
-            depression_count = depression_count.append(store['depression_count'])
-            ckd_count = ckd_count.append(store['ckd_count'])
-            lipid_count = lipid_count.append(store['lipid_count'])
-            diabetes_count = diabetes_count.append(store['diabetes_count'])
-            hypertension_count = hypertension_count.append(store['hypertension_count'])
+            if comorbidity:
+                smoking_count = smoking_count.append(store['smoking_count'])
+                hcv_count = hcv_count.append(store['hcv_count'])
+                anxiety_count = anxiety_count.append(store['anxiety_count'])
+                depression_count = depression_count.append(store['depression_count'])
+                ckd_count = ckd_count.append(store['ckd_count'])
+                lipid_count = lipid_count.append(store['lipid_count'])
+                diabetes_count = diabetes_count.append(store['diabetes_count'])
+                hypertension_count = hypertension_count.append(store['hypertension_count'])
 
-            dead = dead.append(store['dead'])
-            smoking_dead = smoking_dead.append(store['smoking_dead'])
-            hcv_dead = hcv_dead.append(store['hcv_dead'])
-            anxiety_dead = anxiety_dead.append(store['anxiety_dead'])
-            depression_dead = depression_dead.append(store['depression_dead'])
-            ckd_dead = ckd_dead.append(store['ckd_dead'])
-            lipid_dead = lipid_dead.append(store['lipid_dead'])
-            diabetes_dead = diabetes_dead.append(store['diabetes_dead'])
-            hypertension_dead = hypertension_dead.append(store['hypertension_dead'])
+                dead = dead.append(store['dead'])
+                smoking_dead = smoking_dead.append(store['smoking_dead'])
+                hcv_dead = hcv_dead.append(store['hcv_dead'])
+                anxiety_dead = anxiety_dead.append(store['anxiety_dead'])
+                depression_dead = depression_dead.append(store['depression_dead'])
+                ckd_dead = ckd_dead.append(store['ckd_dead'])
+                lipid_dead = lipid_dead.append(store['lipid_dead'])
+                diabetes_dead = diabetes_dead.append(store['diabetes_dead'])
+                hypertension_dead = hypertension_dead.append(store['hypertension_dead'])
 
-            greater_than_zero_count = greater_than_zero_count.append(store['greater_than_zero_count'])
-            greater_than_one_count = greater_than_one_count.append(store['greater_than_one_count'])
-            """
+                comorbid_zero_count = comorbid_zero_count.append(store['comorbid_zero_count'])
+                comorbid_one_count = comorbid_one_count.append(store['comorbid_one_count'])
+                comorbid_two_count = comorbid_two_count.append(store['comorbid_two_count'])
+                comorbid_three_count = comorbid_three_count.append(store['comorbid_three_count'])
+                comorbid_four_count = comorbid_four_count.append(store['comorbid_four_count'])
+
 
     dead_in_care_age = dead_in_care_age.loc[dead_in_care_age.age <= 85]
     dead_out_care_age = dead_out_care_age.loc[dead_out_care_age.age <= 85]
@@ -145,30 +153,32 @@ for group_name in group_names:
 
     feather.write_dataframe(random_params, f'{feather_dir}/{group_name}_random_params.feather')
 
-    """
-    feather.write_dataframe(smoking_count, f'{feather_dir}/{group_name}_smoking_count.feather')
-    feather.write_dataframe(hcv_count, f'{feather_dir}/{group_name}_hcv_count.feather')
-    feather.write_dataframe(anxiety_count, f'{feather_dir}/{group_name}_anxiety_count.feather')
-    feather.write_dataframe(depression_count, f'{feather_dir}/{group_name}_depression_count.feather')
-    feather.write_dataframe(ckd_count, f'{feather_dir}/{group_name}_ckd_count.feather')
-    feather.write_dataframe(lipid_count, f'{feather_dir}/{group_name}_lipid_count.feather')
-    feather.write_dataframe(diabetes_count, f'{feather_dir}/{group_name}_diabetes_count.feather')
-    feather.write_dataframe(hypertension_count, f'{feather_dir}/{group_name}_hypertension_count.feather')
+    if comorbidity:
 
-    feather.write_dataframe(dead, f'{feather_dir}/{group_name}_dead.feather')
-    feather.write_dataframe(smoking_dead, f'{feather_dir}/{group_name}_smoking_dead.feather')
-    feather.write_dataframe(hcv_dead, f'{feather_dir}/{group_name}_hcv_dead.feather')
-    feather.write_dataframe(anxiety_dead, f'{feather_dir}/{group_name}_anxiety_dead.feather')
-    feather.write_dataframe(depression_dead, f'{feather_dir}/{group_name}_depression_dead.feather')
-    feather.write_dataframe(ckd_dead, f'{feather_dir}/{group_name}_ckd_dead.feather')
-    feather.write_dataframe(lipid_dead, f'{feather_dir}/{group_name}_lipid_dead.feather')
-    feather.write_dataframe(diabetes_dead, f'{feather_dir}/{group_name}_diabetes_dead.feather')
-    feather.write_dataframe(hypertension_dead, f'{feather_dir}/{group_name}_hypertension_dead.feather')
+        feather.write_dataframe(smoking_count, f'{feather_dir}/{group_name}_smoking_count.feather')
+        feather.write_dataframe(hcv_count, f'{feather_dir}/{group_name}_hcv_count.feather')
+        feather.write_dataframe(anxiety_count, f'{feather_dir}/{group_name}_anxiety_count.feather')
+        feather.write_dataframe(depression_count, f'{feather_dir}/{group_name}_depression_count.feather')
+        feather.write_dataframe(ckd_count, f'{feather_dir}/{group_name}_ckd_count.feather')
+        feather.write_dataframe(lipid_count, f'{feather_dir}/{group_name}_lipid_count.feather')
+        feather.write_dataframe(diabetes_count, f'{feather_dir}/{group_name}_diabetes_count.feather')
+        feather.write_dataframe(hypertension_count, f'{feather_dir}/{group_name}_hypertension_count.feather')
 
-    feather.write_dataframe(greater_than_zero_count, f'{feather_dir}/{group_name}_greater_than_zero_count.feather')
-    feather.write_dataframe(greater_than_one_count, f'{feather_dir}/{group_name}_greater_than_one_count.feather')
-    """
-
+        feather.write_dataframe(dead, f'{feather_dir}/{group_name}_dead.feather')
+        feather.write_dataframe(smoking_dead, f'{feather_dir}/{group_name}_smoking_dead.feather')
+        feather.write_dataframe(hcv_dead, f'{feather_dir}/{group_name}_hcv_dead.feather')
+        feather.write_dataframe(anxiety_dead, f'{feather_dir}/{group_name}_anxiety_dead.feather')
+        feather.write_dataframe(depression_dead, f'{feather_dir}/{group_name}_depression_dead.feather')
+        feather.write_dataframe(ckd_dead, f'{feather_dir}/{group_name}_ckd_dead.feather')
+        feather.write_dataframe(lipid_dead, f'{feather_dir}/{group_name}_lipid_dead.feather')
+        feather.write_dataframe(diabetes_dead, f'{feather_dir}/{group_name}_diabetes_dead.feather')
+        feather.write_dataframe(hypertension_dead, f'{feather_dir}/{group_name}_hypertension_dead.feather')
+        
+        feather.write_dataframe(comorbid_zero_count, f'{feather_dir}/{group_name}_comorbid_zero_count.feather')
+        feather.write_dataframe(comorbid_one_count, f'{feather_dir}/{group_name}_comorbid_one_count.feather')
+        feather.write_dataframe(comorbid_two_count, f'{feather_dir}/{group_name}_comorbid_two_count.feather')
+        feather.write_dataframe(comorbid_three_count, f'{feather_dir}/{group_name}_comorbid_three_count.feather')
+        feather.write_dataframe(comorbid_four_count, f'{feather_dir}/{group_name}_comorbid_four_count.feather')
     
 
 
