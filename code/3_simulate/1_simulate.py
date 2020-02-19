d89c67f14660fdbad7464696194236ef1bf4da67
Cameron Stewart <cameron.stewart@jhu.edu>
2020-02-19 16:00:37 -0500

    Add Incidence Reporting With Denominator

--- code/3_simulate/1_simulate.py
+++ code/3_simulate/1_simulate.py
@@ -12,7 +12,7 @@ def run(parameters, group_name, replication):
     return True
 
 
-ray.init(num_cpus=4)
+ray.init(num_cpus=1)
 param_file = getcwd() + '/../../data/parameters/parameters.h5'
 replications = range(1)
 
@@ -20,7 +20,8 @@ group_names = ['msm_white_male', 'msm_black_male', 'msm_hisp_male', 'idu_white_m
                'idu_hisp_male', 'idu_white_female', 'idu_black_female', 'idu_hisp_female', 'het_white_male',
                'het_black_male', 'het_hisp_male', 'het_white_female', 'het_black_female', 'het_hisp_female']
 
-group_names = ['msm_hisp_male']
+group_names = ['msm_white_male']
+
 
 for group_name in group_names:
     print(group_name)
