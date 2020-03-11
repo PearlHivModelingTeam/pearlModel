b2f47e0e65921da760cb8ffcc87563165c1bff3b
Cameron Stewart <cameron.stewart@jhu.edu>
2020-03-11 15:25:40 -0400

    bug

--- code/3_simulate/pearl.py
+++ code/3_simulate/pearl.py
@@ -1052,7 +1052,7 @@ class Pearl:
             self.kill_out_care()  # Kill some people out of care
             self.reengage()  # Reengage some people out of care
 
-            #self.lose_to_follow_up()
+            self.lose_to_follow_up()
 
             # Record output statistics
             self.record_stats()
