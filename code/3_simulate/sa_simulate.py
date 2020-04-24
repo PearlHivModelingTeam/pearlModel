7a4d44b55a1f67b9fae98655267c5435119150b3
Cameron Stewart <cameron.stewart@jhu.edu>
2020-04-23 21:39:12 -0400

    Dedicated Sensitivity Analysis Script

--- code/3_simulate/sa_simulate.py
+++ code/3_simulate/sa_simulate.py
@@ -18,7 +18,7 @@ ray.init(num_cpus=7)
 
 # Input and output files
 param_file = f'{os.getcwd()}/../../data/parameters/parameters.h5'
-output_folder = f'{os.getcwd()}/../../out/raw'
+output_folder_base = f'{os.getcwd()}/../../out/sa'
 
 # Number of replications
 replications = range(1)
@@ -29,11 +29,11 @@ group_names = ['msm_white_male', 'msm_black_male', 'msm_hisp_male', 'idu_white_m
                'het_black_male', 'het_hisp_male', 'het_white_female', 'het_black_female', 'het_hisp_female']
 
 group_names = ['msm_white_male', 'msm_black_male', 'msm_hisp_male']
-group_names = ['het_hisp_female']
+#group_names = ['het_hisp_female']
 
 
 # Declare sensitivity analysis params
-sa_dict = {'lambda1':               None,
+sa_base = {'lambda1':               None,
            'mu1':                   None,
            'mu2':                   None,
            'sigma1':                None,
@@ -45,20 +45,28 @@ sa_dict = {'lambda1':               None,
            'cd4_decrease':          None,
            'new_pop_size':          None}
 
-# Delete old files
-if os.path.isdir(output_folder):
-    for filename in os.listdir(output_folder):
-        file_path = os.path.join(output_folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+for key in sa_base:
+    print(key)
+    for param in [0, 1]:
+        print(param)
+        output_folder = f'{output_folder_base}/{key}_{param}'
+        sa_dict = sa_base.copy()
+        sa_dict[key] = param
 
-# Run simulations
-for group_name in group_names:
-    print(group_name)
-    futures = [run.remote(pearl.Parameters(path=param_file, group_name=group_name, comorbidity_flag=0, dx_reduce_flag=0,
-                                           sa_dict=sa_dict, output_folder=output_folder), group_name, replication)
-               for replication in replications]
-    ray.get(futures)
+        # Delete old files
+        if os.path.isdir(output_folder):
+            for filename in os.listdir(output_folder):
+                file_path = os.path.join(output_folder, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                except Exception as e:
+                    print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+        # Run simulations
+        for group_name in group_names:
+            print(group_name)
+            futures = [run.remote(pearl.Parameters(path=param_file, group_name=group_name, comorbidity_flag=0, dx_reduce_flag=0,
+                                                   sa_dict=sa_dict, output_folder=output_folder), group_name, replication)
+                       for replication in replications]
+            ray.get(futures)
