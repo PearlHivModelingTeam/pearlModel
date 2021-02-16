464cb239a5ed0832adcfff7c9c18083fa5ca12c3
Cameron Stewart <cameron.n.stewart@gmail.com>
2021-02-16 15:11:55 -0500

    Test commit for commit hash checking

--- code/3_simulate/1_simulate.py
+++ code/3_simulate/1_simulate.py
@@ -1,9 +1,12 @@
 # Imports
 import os
+import platform
 import ray
 import pearl
 import yaml
 import shutil
+import pkg_resources
+import subprocess
 from pathlib import Path
 import argparse
 from datetime import datetime
@@ -16,31 +19,61 @@ def run(parameters, group_name, replication):
     return simulation.stats
 
 
+# Define the argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('--config')
+parser.add_argument('--rerun')
 args = parser.parse_args()
-if args.config:
-    yaml_file = Path(f'config/{args.config}')
-else:
-    yaml_file = Path('config/test.yaml')
 
+pearl_dir = Path('../..')
 date_string = datetime.today().strftime('%Y-%m-%d')
-output_folder = Path(f'../../out/{yaml_file.stem}_{date_string}/')
-param_file = Path(f'../../param/parameters.h5')
+param_file = Path(f'{pearl_dir}/param/parameters.h5')
 
-# Load parameters
-with open(yaml_file, 'r') as f:
-    param_yaml = yaml.load(f, Loader=yaml.FullLoader)
-    num_cpus = param_yaml['num_cpus']
-    replications = range(param_yaml['replications'])
-    group_names = param_yaml['group_names']
-    sa_dict = param_yaml['sa_dict']
-    comorbidity_flag = param_yaml['comorbidity_flag']
-    mm_detail_flag = param_yaml['mm_detail_flag']
-    smoking_intervention = param_yaml['smoking_intervention']
-    new_dx = param_yaml['new_dx']
-    record_tv_cd4 = param_yaml['record_tv_cd4']
-    verbose = param_yaml['verbose']
+# Check that requirements.txt is met
+with open(f'{pearl_dir}/requirements.txt', 'r') as requirements:
+    pkg_resources.require(requirements)
+
+rerun_folder = None
+if args.config:
+    config_file = Path(f'config/{args.config}')
+    output_folder = Path(f'{pearl_dir}/out/{config_file.stem}_{date_string}/')
+elif args.rerun:
+    rerun_folder = Path(f'{pearl_dir}/out/{args.rerun}')
+    config_file = Path(f'{rerun_folder}/rerun.yaml')
+    output_folder = Path(f'{pearl_dir}/out/{args.rerun}_rerun_{date_string}/')
+else:
+    config_file = Path('config/test.yaml')
+    output_folder = Path(f'{pearl_dir}/out/{config_file.stem}_{date_string}/')
+
+
+# Load config_file
+with open(config_file, 'r') as yaml_file:
+    config_yaml = yaml.safe_load(yaml_file)
+
+num_cpus = config_yaml['num_cpus']
+replications = range(config_yaml['replications'])
+group_names = config_yaml['group_names']
+sa_dict = config_yaml['sa_dict']
+comorbidity_flag = config_yaml['comorbidity_flag']
+mm_detail_flag = config_yaml['mm_detail_flag']
+smoking_intervention = config_yaml['smoking_intervention']
+new_dx = config_yaml['new_dx']
+record_tv_cd4 = config_yaml['record_tv_cd4']
+verbose = config_yaml['verbose']
+if args.rerun:
+    print('rerun')
+    python_version = config_yaml['python_version']
+    if python_version != platform.python_version():
+        raise EnvironmentError("Incorrect python version for rerun")
+    commit_hash = config_yaml['commit_hash']
+    if commit_hash != subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip():
+        raise EnvironmentError("Incorrect commit hash for rerun")
+else:
+    config_yaml['python_version'] = platform.python_version()
+    print(config_yaml['python_version'])
+    config_yaml['commit_hash'] = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+    print(config_yaml['commit_hash'])
+
 
 # Delete old files
 if os.path.isdir(output_folder):
@@ -53,16 +86,20 @@ if os.path.isdir(output_folder):
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 else:
     os.makedirs(output_folder)
+    os.makedirs(f'{output_folder}/random_states')
 
+# Copy config file to output dir
 
-shutil.copyfile(yaml_file, f'{output_folder}/{yaml_file.stem}.yaml')
+with open(f'{output_folder}/rerun.yaml', 'w') as yaml_file:
+    yaml.safe_dump(config_yaml, yaml_file)
+
 
 # Run simulations
 ray.init(num_cpus=num_cpus)
 out_list = []
 for group_name in group_names:
     print(group_name)
-    parameters = pearl.Parameters(path=param_file, group_name=group_name, replications=replications, comorbidity_flag=comorbidity_flag,
+    parameters = pearl.Parameters(path=param_file, rerun_folder=rerun_folder, group_name=group_name, replications=replications, comorbidity_flag=comorbidity_flag,
                                   mm_detail_flag=mm_detail_flag, sa_dict=sa_dict, new_dx=new_dx,
                                   output_folder=output_folder, record_tv_cd4=record_tv_cd4, verbose=verbose,
                                   smoking_intervention=smoking_intervention)
