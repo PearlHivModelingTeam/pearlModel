508650febe56b6bd0a3d7d1a9d043d1284720031
Parastu <pkasaie@jhu.edu>
2023-11-20 15:40:42 -0500

    debugging

--- code/2_simulate_server.py
+++ code/2_simulate_server.py
@@ -16,13 +16,13 @@ import os
 def run(group_name_run, replication_run):
     replication_run_str = str(replication_run).zfill(len(str(config['replications'])))
     output_path1 = output_root_path/'csv_output'/f"{group_name_run}/bmi_{config['bmi_intervention']}/replication_{replication_run}"
-    rerun_path = rerun_root_path/'csv_output'/group_name_run/f'replication_{replication_run_str}' if rerun_root_path is not None else None
+    rerun_path = rerun_root_path/'csv_output'/f"group_name_run/replication_{replication_run_str}" if rerun_root_path is not None else None
     parameters = pearl.Parameters(path=param_file_path, rerun_folder=rerun_path, output_folder=output_path1,
                                   group_name=group_name_run, comorbidity_flag=config['comorbidity_flag'], new_dx=config['new_dx'],
                                   final_year=config['final_year'], mortality_model=config['mortality_model'],
                                   mortality_threshold_flag=config['mortality_threshold_flag'], idu_threshold=config['idu_threshold'],
                                   verbose=config['verbose'], bmi_intervention=config['bmi_intervention'], bmi_intervention_probability=config['bmi_intervention_probability'])
-    print(f'group {group_name_run}: output set to {parameters.output_folder}')
+    print(f'Initializing group {group_name_run}: output set to {parameters.output_folder}')
     pearl.Pearl(parameters, group_name_run, replication_run)
     print(f'simulation finished for {group_name_run},rep= {replication_run}, output saved in {output_path1.resolve()}')
 
