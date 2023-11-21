8bc902265ce80e7c2bf62422c286b69a4649ef96
Parastu <pkasaie@jhu.edu>
2023-11-21 10:34:39 -0500

    testing new output path

--- code/2_simulate_server.py
+++ code/2_simulate_server.py
@@ -15,34 +15,34 @@ import os
 @ray.remote
 def run(group_name_run, replication_run):
     replication_run_str = str(replication_run).zfill(len(str(config['replications'])))
-    output_path1 = output_root_path/'csv_output'/f"{group_name_run}/bmi_{config['bmi_intervention']}/replication_{replication_run_str}"
-    rerun_path = rerun_root_path/'csv_output'/f"group_name_run/replication_{replication_run_str}" if rerun_root_path is not None else None
-    parameters = pearl.Parameters(path=param_file_path, rerun_folder=rerun_path, output_folder=output_path1,
+    out_path = f"csv_output/bmi_{config['bmi_intervention']}/{group_name_run}/replication_{replication_run_str}" #setting up the path name
+    output_folder = output_root_path/out_path
+    rerun_folder = rerun_root_path/out_path if rerun_root_path is not None else None
+    parameters = pearl.Parameters(path=param_file_path, rerun_folder=rerun_folder, output_folder=output_folder,
                                   group_name=group_name_run, comorbidity_flag=config['comorbidity_flag'], new_dx=config['new_dx'],
                                   final_year=config['final_year'], mortality_model=config['mortality_model'],
                                   mortality_threshold_flag=config['mortality_threshold_flag'], idu_threshold=config['idu_threshold'],
                                   verbose=config['verbose'], bmi_intervention=config['bmi_intervention'], bmi_intervention_probability=config['bmi_intervention_probability'])
     print(f'Initializing group {group_name_run}, rep {replication_run}: output set to {parameters.output_folder}')
     pearl.Pearl(parameters, group_name_run, replication_run)
-    print(f'simulation finished for {group_name_run},rep= {replication_run}, output saved in {output_path1.resolve()}')
+    print(f'simulation finished for {group_name_run},rep= {replication_run}')
 
 
-@ray.remote
-def run_sa(sa_variable_run, sa_value_run, group_name_run, replication_run):
-    replication_run_str = str(replication_run).zfill(len(str(config['replications'])))
-    output_path = output_root_path/'csv_output'/f'{sa_variable_run}_{sa_value_run}'/group_name_run/f'replication_{replication_run_str}'
-    rerun_path = rerun_root_path/'csv_output'/f'{sa_variable_run}_{sa_value_run}'/group_name_run/f'replication_{replication_run_str}' if rerun_root_path is not None else None
-    parameters = pearl.Parameters(path=param_file_path, rerun_folder=rerun_path, output_folder=output_path,
-                                  group_name=group_name_run, comorbidity_flag=config['comorbidity_flag'], new_dx=config['new_dx'],
-                                  final_year=config['final_year'], mortality_model=config['mortality_model'],
-                                  mortality_threshold_flag=config['mortality_threshold_flag'], idu_threshold=config['idu_threshold'],
-                                  verbose=config['verbose'], sa_type=config['sa_type'], sa_variable=sa_variable_run,
-                                  sa_value=sa_value_run, bmi_intervention=config['bmi_intervention'], bmi_intervention_probability=config['bmi_intervention_probability'])
-    pearl.Pearl(parameters, group_name_run, replication_run)
+# @ray.remote
+# def run_sa(sa_variable_run, sa_value_run, group_name_run, replication_run):
+#     replication_run_str = str(replication_run).zfill(len(str(config['replications'])))
+#     output_path = output_root_path/'csv_output'/f'{sa_variable_run}_{sa_value_run}'/group_name_run/f'replication_{replication_run_str}'
+#     rerun_path = rerun_root_path/'csv_output'/f'{sa_variable_run}_{sa_value_run}'/group_name_run/f'replication_{replication_run_str}' if rerun_root_path is not None else None
+#     parameters = pearl.Parameters(path=param_file_path, rerun_folder=rerun_path, output_folder=output_path,
+#                                   group_name=group_name_run, comorbidity_flag=config['comorbidity_flag'], new_dx=config['new_dx'],
+#                                   final_year=config['final_year'], mortality_model=config['mortality_model'],
+#                                   mortality_threshold_flag=config['mortality_threshold_flag'], idu_threshold=config['idu_threshold'],
+#                                   verbose=config['verbose'], sa_type=config['sa_type'], sa_variable=sa_variable_run,
+#                                   sa_value=sa_value_run, bmi_intervention=config['bmi_intervention'], bmi_intervention_probability=config['bmi_intervention_probability'])
+#     pearl.Pearl(parameters, group_name_run, replication_run)
 
-
+###############################
 start_time = datetime.now()
-
 # Define the argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('--config')
@@ -71,7 +71,6 @@ elif args.rerun:
 else:
     config_file_path = pearl_path/'config/test.yaml'
     output_root_path = pearl_path/f'out/{config_file_path.stem}_{date_string}'
-
 # Load config_file
 with open(config_file_path, 'r') as config_file:
     config = yaml.safe_load(config_file)
@@ -93,20 +92,20 @@ else:
 if config['sa_type'] == 'none':
     sa_variables = None
     sa_values = None
-elif config['sa_type'] == 'type1':
-    sa_variables = pearl.sa_type1_var
-    sa_values = ['low', 'high']
-elif config['sa_type'] == 'type2':
-    sa_variables = pearl.sa_type2_var
-    sa_values = [0.8, 1.2]
-elif (config['sa_type'] == 'aim2_inc') | (config['sa_type'] == 'aim2_prev'):
-    sa_variables = pearl.sa_aim2_var
-    sa_values = [0.75, 1.25]
-elif config['sa_type'] == 'aim2_mort':
-    sa_variables = pearl.sa_aim2_mort_var
-    sa_values = [0.75, 1.25]
-else:
-    raise ValueError("Unrecognized sensitivity analysis type")
+# elif config['sa_type'] == 'type1':
+#     sa_variables = pearl.sa_type1_var
+#     sa_values = ['low', 'high']
+# elif config['sa_type'] == 'type2':
+#     sa_variables = pearl.sa_type2_var
+#     sa_values = [0.8, 1.2]
+# elif (config['sa_type'] == 'aim2_inc') | (config['sa_type'] == 'aim2_prev'):
+#     sa_variables = pearl.sa_aim2_var
+#     sa_values = [0.75, 1.25]
+# elif config['sa_type'] == 'aim2_mort':
+#     sa_variables = pearl.sa_aim2_mort_var
+#     sa_values = [0.75, 1.25]
+# else:
+#     raise ValueError("Unrecognized sensitivity analysis type")
 
 # Create Output folder structure
 if output_root_path.is_dir():
@@ -120,16 +119,17 @@ if sa_variables is None:
     for group_name in config['group_names']:
         for replication in range(config['replications']):
             replication_str = str(replication).zfill(len(str(config['replications'])))
-            output_path = output_root_path/'csv_output'/group_name/f"bmi_{config['bmi_intervention']}/replication_{replication_str}"
-            output_path.mkdir(parents=True)
-else:
-    for sa_variable in sa_variables:
-        for sa_value in sa_values:
-            for group_name in config['group_names']:
-                for replication in range(config['replications']):
-                    replication_str = str(replication).zfill(len(str(config['replications'])))
-                    output_path = output_root_path/'csv_output'/f'{sa_variable}_{sa_value}'/group_name/f'replication_{replication_str}'
-                    output_path.mkdir(parents=True)
+            out_path = f"csv_output/bmi_{config['bmi_intervention']}/{group_name_run}/replication_{replication_run_str}" #setting up the path name
+            output_folder = output_root_path/out_path
+            output_folder.mkdir(parents=True)
+# else:
+#     for sa_variable in sa_variables:
+#         for sa_value in sa_values:
+#             for group_name in config['group_names']:
+#                 for replication in range(config['replications']):
+#                     replication_str = str(replication).zfill(len(str(config['replications'])))
+#                     output_path = output_root_path/'csv_output'/f'{sa_variable}_{sa_value}'/group_name/f'replication_{replication_str}'
+#                     output_path.mkdir(parents=True)
 
 # Copy config file to output dir
 with open(output_root_path/'config.yaml', 'w') as yaml_file:
@@ -143,17 +143,19 @@ try:
     ray.init(num_cpus=config['num_cpus'])
 except Exception as e:
     print(f"Error initializing Ray: {e}")
+
+# Lanching simulations
 if sa_variables is None:
     print("running main analysis...")
     ray.get([run.remote(group_name, replication)
              for group_name in config['group_names']
              for replication in range(config['replications'])])
-else:
-    ray.get([run_sa.remote(sa_variable, sa_value, group_name, replication)
-             for sa_variable in sa_variables
-             for sa_value in sa_values
-             for group_name in config['group_names']
-             for replication in range(config['replications'])])
+# else:
+#     ray.get([run_sa.remote(sa_variable, sa_value, group_name, replication)
+#              for sa_variable in sa_variables
+#              for sa_value in sa_values
+#              for group_name in config['group_names']
+#              for replication in range(config['replications'])])
 
 
 end_time = datetime.now()
