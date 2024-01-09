d37c8cd067c960379ea7b55a4b06a67a52c01c38
Parastu <pkasaie@jhu.edu>
2024-01-09 10:38:49 -0500

    added a new script to run scenarios and convert outputs

--- code/2_run_scenarios.py
+++ code/2_run_scenarios.py
@@ -17,22 +17,22 @@ for f in config_files:
     config_file_path = pearl_path/'config'/f
     output_root_path = pearl_path/f'out/{config_file_path.stem}_{date_string}'
     ############
-    print(f"Running simulations outputs ...")
-    # Specify the command to call another Python script
-    command = ["python3", f"{pearl_path}/code/2_simulate_server.py", "--config", f"{config_file_path}","--overwrite"]
-    # Use subprocess to run the command and redirect both output and error to the same file
-    output_and_error_log = f"2_out_{f}.log"
-    with open(output_and_error_log, "w") as log_file:
-        process = subprocess.Popen(command, stdout=log_file, stderr=subprocess.STDOUT)
-    # Wait for the process to finish
-    process.communicate()
-    # Check the exit code
-    exit_code = process.returncode
-    print("simulations ran with exit Code:", exit_code)
+    # print(f"Running simulations in parallel ...")
+    # # Specify the command to call another Python script
+    # command = ["python3", f"{pearl_path}/code/2_simulate_server.py", "--config", f"{config_file_path}","--overwrite"]
+    # # Use subprocess to run the command and redirect both output and error to the same file
+    # output_and_error_log = f"2_out_{f}.log"
+    # with open(output_and_error_log, "w") as log_file:
+    #     process = subprocess.Popen(command, stdout=log_file, stderr=subprocess.STDOUT)
+    # # Wait for the process to finish
+    # process.communicate()
+    # # Check the exit code
+    # exit_code = process.returncode
+    # print("simulations ran with exit Code:", exit_code)
     ############
     print(f"Creating HDF outputs ...")
     # Specify the command to call another Python script
-    command = ["python3", f"{pearl_path}/code/3_convert_csv_to_hdf.py", "--arg1", f"{output_root_path}"]
+    command = ["python3", f"{pearl_path}/code/3_convert_csv_to_hdf.py","--dir", f"{output_root_path}"]
     output_and_error_log = f"3_out_{f}.log"
     with open(output_and_error_log, "w") as log_file:
         process = subprocess.Popen(command, stdout=log_file, stderr=subprocess.STDOUT)
