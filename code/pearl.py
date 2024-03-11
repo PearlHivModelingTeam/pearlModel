8ec647961103e27ce3f37e9124a535b467ad39b5
Parastu <pkasaie@jhu.edu>
2024-03-11 12:54:51 -0400

    fixed a bug with timeline of bmi intervention to capture the last year for receiving the intervention

--- code/pearl.py
+++ code/pearl.py
@@ -431,13 +431,13 @@ def simulate_new_dx(new_dx, linkage_to_care):
     return n_initial_nonusers, new_agents
 
 
-def apply_bmi_intervention(pop, parameters):
+def apply_bmi_intervention(pop, parameters): #pop is a copy of the new population of ART initiators that are starting ART in this year
     if parameters.bmi_intervention == 0:
         raise ValueError('Running apply_bmi_intervention despite bmi_intervention=0')
     pop['bmiInt_scenario'] = parameters.bmi_intervention_scenario
-    pop['bmiInt_year'] = pop['h1yy'].isin(range(parameters.bmi_intervention_start_year, parameters.bmi_intervention_end_year))
+    pop['bmiInt_year'] = pop['h1yy'].isin(range(parameters.bmi_intervention_start_year, parameters.bmi_intervention_end_year+1)) # this function doesnt capture the last value, so we do +1
     pop['bmiInt_coverage'] = np.random.choice([1, 0], size=len(pop), replace=True,
-                                              p=[parameters.bmi_intervention_coverage,
+                                              p=[parameters.bmi_intervention_coverage, #prob of choosing the value of 1
                                                  1 - parameters.bmi_intervention_coverage])
     pop['bmiInt_effectiveness'] = np.random.choice([1, 0], size=len(pop), replace=True,
                                                    p=[parameters.bmi_intervention_effectiveness,
