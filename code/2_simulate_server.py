5f9870bd35e8332130c48ac31a378abd2c41201c
Kendall Reid <kreid415@gmail.com>
2024-06-25 09:02:31 -0400

    experimenting with dask delayed

--- code/2_simulate_server.py
+++ code/2_simulate_server.py
@@ -15,7 +15,7 @@ from datetime import datetime
 import os
 import math
 
-@dask.delayed
+
 def run(group_name_run, replication_run):
     replication_run_str = str(replication_run).zfill(len(str(config['replications'])))
     out_path = f"csv_output/{group_name_run}/replication_{replication_run_str}" #setting up the path name
@@ -44,7 +44,7 @@ def run(group_name_run, replication_run):
 ###############################
 
 if __name__ == '__main__':
-    #max_workers=30
+    max_workers=30
     print("1", flush=True)
     #client = Client(n_workers=max_workers)
 
@@ -133,7 +133,7 @@ if __name__ == '__main__':
         results = []
         for group_name_run in config['group_names']:
             for replication_run in range(config['replications']):
-                results.append(run(group_name_run, replication_run))
+                results.append(dask.delayed(run)(group_name_run, replication_run))
     
         dask.compute(results)
     
