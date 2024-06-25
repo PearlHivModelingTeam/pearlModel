74e7939f1a6d8efbf78f066a9ffb609fc361f967
Kendall Reid <kreid415@gmail.com>
2024-06-25 09:05:25 -0400

    try with delayed and processes

--- code/2_simulate_server.py
+++ code/2_simulate_server.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
             for replication_run in range(config['replications']):
                 results.append(run(group_name_run, replication_run))
     
-        dask.compute(results)
+        dask.compute(results, scheduler="processes")
     
     end_time = datetime.now()
     print(f'**** Elapsed Time: {end_time - start_time} ****')
