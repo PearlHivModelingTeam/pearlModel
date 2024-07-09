230bcb50d11e315a8cfb30293bee7cb88fb7dbb8
Kendall Reid <kreid415@gmail.com>
2024-07-09 12:19:08 -0400

    fix bmi thresholds

--- code/pearl.py
+++ code/pearl.py
@@ -478,9 +478,9 @@ def apply_bmi_intervention(pop, parameters):
                                                       1 - parameters.bmi_intervention_effectiveness])
     # determine eligibility:
     pop['bmiInt_ineligible_dm'] = (pop['dm'] == 1)
-    pop['bmiInt_ineligible_underweight'] = (pop['pre_art_bmi'] < 18.5)
-    pop['bmiInt_ineligible_obese'] = (pop['pre_art_bmi'] > 30)
-    pop['bmiInt_eligible'] = (pop['pre_art_bmi'] >= 18.5) & (pop['pre_art_bmi'] <= 30) & (pop['dm'] == 0)
+    pop['bmiInt_ineligible_underweight'] = (pop['pre_art_bmi'] <= 18.5)
+    pop['bmiInt_ineligible_obese'] = (pop['pre_art_bmi'] >= 30)
+    pop['bmiInt_eligible'] = (pop['pre_art_bmi'] > 18.5) & (pop['pre_art_bmi'] < 30) & (pop['dm'] == 0)
 
     # eligible people are enrolled in the intervention:
     pop['bmiInt_received'] = pop['bmiInt_eligible'] & pop['bmiInt_year'] & pop['bmiInt_coverage']
@@ -489,7 +489,7 @@ def apply_bmi_intervention(pop, parameters):
     pop['post_art_bmi_without_bmiInt'] = pop['post_art_bmi']
     pop['bmi_increase_postART'] = (pop['post_art_bmi'] / pop['pre_art_bmi'] > 1)
     pop['bmi_increase_postART_over5p']= (pop['post_art_bmi']/pop['pre_art_bmi'] > 1.05)
-    pop['become_obese_postART'] = pop['post_art_bmi_without_bmiInt'] > 30
+    pop['become_obese_postART'] = pop['post_art_bmi_without_bmiInt'] >= 30
 
 
     # Scenario0: no BMI intervention
