9c83df9d36a134e5f1ba76465dde50ed09538d1d
Kendall Reid <kreid415@gmail.com>
2024-09-26 17:29:14 +0000

    fix bmi_sa.py function

--- scripts/7_bmi_sa.py
+++ scripts/7_bmi_sa.py
@@ -1,9 +1,8 @@
 import argparse
-from datetime import datetime
+import datetime
 from pathlib import Path
 
-from matplotlib.lines import Line2D
-from matplotlib.patches import Patch
+from dask import dataframe as dd
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -17,7 +16,9 @@ def calc_tornado_vals(
     param_df_baseline,
     param_df_variable,
     col_name,
-    num_samples,
+    num_samples=1000,
+    lesser=0.1,
+    greater=0.9,
 ):
     sub_param_baseline = param_df_baseline[["replication", "group", col_name]]
     sub_param_variable = param_df_variable[["replication", "group", col_name]]
@@ -30,139 +31,105 @@ def calc_tornado_vals(
     ).fillna(0)
 
     quantile_val_baseline = (
-        baseline_merged.groupby("group")[col_name].quantile([0.25, 0.75]).unstack().reset_index()
+        baseline_merged.groupby("group")[col_name]
+        .quantile([lesser, greater])
+        .unstack()
+        .reset_index()
     )
     quantile_val_variable = (
-        variable_merged.groupby("group")[col_name].quantile([0.25, 0.75]).unstack().reset_index()
+        variable_merged.groupby("group")[col_name]
+        .quantile([lesser, greater])
+        .unstack()
+        .reset_index()
     )
 
-    tornado_df = []
+    difference_tornado_df = []
+    baseline_tornado_df = []
+    variable_tornado_df = []
     for group in quantile_val_baseline["group"].unique():
         group_df_baseline = baseline_merged[baseline_merged["group"] == group]
         group_df_variable = variable_merged[variable_merged["group"] == group]
 
         lesser_val_baseline = quantile_val_baseline[quantile_val_baseline["group"] == group][
-            0.25
+            lesser
         ].values[0]
         greater_val_baseline = quantile_val_baseline[quantile_val_baseline["group"] == group][
-            0.75
+            greater
         ].values[0]
         lesser_val_variable = quantile_val_variable[quantile_val_variable["group"] == group][
-            0.25
+            lesser
         ].values[0]
         greater_val_variable = quantile_val_variable[quantile_val_variable["group"] == group][
-            0.75
+            greater
         ].values[0]
 
-        lesser_group_df_baseline = (
-            group_df_baseline[group_df_baseline[col_name] <= lesser_val_baseline]
-            .sample(num_samples, replace=True)
-            .reset_index()
-        )
-        greater_group_df_baseline = (
-            group_df_baseline[group_df_baseline[col_name] >= greater_val_baseline]
-            .sample(num_samples, replace=True)
-            .reset_index()
-        )
-        lesser_group_df_variable = (
-            group_df_variable[group_df_variable[col_name] <= lesser_val_variable]
-            .sample(num_samples, replace=True)
-            .reset_index()
-        )
-        greater_group_df_variable = (
-            group_df_variable[group_df_variable[col_name] >= greater_val_variable]
-            .sample(num_samples, replace=True)
-            .reset_index()
-        )
+        lesser_group_df_baseline = group_df_baseline[
+            group_df_baseline[col_name] <= lesser_val_baseline
+        ]
+        greater_group_df_baseline = group_df_baseline[
+            group_df_baseline[col_name] >= greater_val_baseline
+        ]
+        lesser_group_df_variable = group_df_variable[
+            group_df_variable[col_name] <= lesser_val_variable
+        ]
+        greater_group_df_variable = group_df_variable[
+            group_df_variable[col_name] >= greater_val_variable
+        ]
 
-        group_tornado_df = {
+        lesser_group_df_baseline_sample = lesser_group_df_baseline.sample(
+            num_samples, replace=True
+        ).reset_index()
+        greater_group_df_baseline_sample = greater_group_df_baseline.sample(
+            num_samples, replace=True
+        ).reset_index()
+        lesser_group_df_variable_sample = lesser_group_df_variable.sample(
+            num_samples, replace=True
+        ).reset_index()
+        greater_group_df_variable_sample = greater_group_df_variable.sample(
+            num_samples, replace=True
+        ).reset_index()
+
+        baseline_group_tornado_df = {
             "group": group,
             "variable": col_name,
-            0.25: (lesser_group_df_variable["risk"] - lesser_group_df_baseline["risk"]).median(),
-            0.75: (greater_group_df_variable["risk"] - greater_group_df_baseline["risk"]).median(),
+            lesser: (lesser_group_df_baseline_sample["risk"]).median(),
+            greater: (greater_group_df_baseline_sample["risk"]).median(),
+            "lesser_count": lesser_group_df_baseline["risk"].count(),
+            "greater_count": greater_group_df_baseline["risk"].count(),
         }
-        tornado_df.append(group_tornado_df)
+        baseline_tornado_df.append(baseline_group_tornado_df)
+        variable_group_tornado_df = {
+            "group": group,
+            "variable": col_name,
+            lesser: (lesser_group_df_variable_sample["risk"]).median(),
+            greater: (greater_group_df_variable_sample["risk"]).median(),
+            "lesser_count": lesser_group_df_variable["risk"].count(),
+            "greater_count": greater_group_df_variable["risk"].count(),
+        }
+        variable_tornado_df.append(variable_group_tornado_df)
+        difference_group_tornado_df = {
+            "group": group,
+            "variable": col_name,
+            lesser: (
+                lesser_group_df_variable_sample["risk"] - lesser_group_df_baseline_sample["risk"]
+            ).median(),
+            greater: (
+                greater_group_df_variable_sample["risk"] - greater_group_df_baseline_sample["risk"]
+            ).median(),
+        }
+        difference_tornado_df.append(difference_group_tornado_df)
 
-    tornado_df = pd.DataFrame(tornado_df)
-    return tornado_df
+    baseline_tornado_df = pd.DataFrame(baseline_tornado_df)
+    variable_tornado_df = pd.DataFrame(variable_tornado_df)
+    difference_tornado_df = pd.DataFrame(difference_tornado_df)
+    return baseline_tornado_df, variable_tornado_df, difference_tornado_df
 
 
-if __name__ == "__main__":
-    start_time = datetime.now()
-
-    # Define the argument parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--baseline")
-    parser.add_argument("--variable")
-    parser.add_argument("--out_dir")
-    parser.add_argument("--num_samples", default=1000, type=int)
-    args = parser.parse_args()
-
-    baseline_dir = Path(args.baseline)
-    variable_dir = Path(args.variable)
-    out_dir = Path(args.out_dir)
-
-    param_df_baseline = pd.read_parquet(baseline_dir / "parameters.parquet").reset_index(drop=True)
-    param_df_variable = pd.read_parquet(variable_dir / "parameters.parquet").reset_index(drop=True)
-
-    bmi_int_dm_prev_variable = pd.read_parquet(variable_dir / "bmi_int_dm_prev.parquet")
-    bmi_int_dm_prev_baseline = pd.read_parquet(baseline_dir / "bmi_int_dm_prev.parquet")
-
-    # clean to control specifications
-    control_bmi_int_dm_prev_variable = clean_control(bmi_int_dm_prev_variable, only_eligible=True)
-    control_bmi_int_dm_prev_baseline = clean_control(bmi_int_dm_prev_baseline, only_eligible=True)
-
-    # filter for only people eligible for intervention
-    bmi_int_eligible_risk_variable = calc_risk_by_group(
-        control_bmi_int_dm_prev_variable, 7
-    ).compute()
-    bmi_int_eligible_risk_baseline = calc_risk_by_group(
-        control_bmi_int_dm_prev_baseline, 7
-    ).compute()
-
-    num_samples = args.num_samples
-
-    baseline_risk = (
-        bmi_int_eligible_risk_baseline.groupby("group")
-        .sample(num_samples, replace=True)
-        .reset_index()
-    )
-    variable_risk = (
-        bmi_int_eligible_risk_variable.groupby("group")
-        .sample(num_samples, replace=True)
-        .reset_index()
-    )
-
-    risk_df = pd.DataFrame(variable_risk["risk"] - baseline_risk["risk"])
-    risk_df["group"] = baseline_risk["group"]
-
-    baseline_risk_df = risk_df.groupby("group").median().reset_index()
-
-    target_columns = [
-        "prev_users_dict_dm",
-        "prev_inits_dict_dm",
-        "sa_incidence_shift_dm",
-        "pre_art_bmi",
-        "post_art_bmi",
-        "art_initiators",
-    ]
-
-    tornado_vals = []
-    for col in target_columns:
-        tornado_vals.append(
-            calc_tornado_vals(
-                bmi_int_eligible_risk_baseline,
-                bmi_int_eligible_risk_variable,
-                param_df_baseline,
-                param_df_variable,
-                col,
-                num_samples,
-            )
-        )
-    tornado_vals = pd.concat(tornado_vals).reset_index(drop=True)
-
+def tornado_plot(tornado_vals, baseline_vals):
+    # create an axis
     fig, axs = plt.subplots(5, 3, figsize=(40, 20))
 
+    # get the groups for plotting and sort them
     plot_groups = np.sort(tornado_vals.group.unique())
 
     for i, group in enumerate(plot_groups):
@@ -174,42 +141,235 @@ if __name__ == "__main__":
         ys = range(len(y_tick_label))[::-1]
         height = 0.8
 
-        color_5 = "#0d47a1"
-        color_95 = "#e2711d"
+        color_lesser = "#0d47a1"
+        color_greater = "#e2711d"
         color_line = "#2ECC71"
 
         # Data to be visualized
-        value_25 = group_vals.iloc[:, 2].values
-        value_75 = group_vals.iloc[:, 3].values
-        base = baseline_risk_df[baseline_risk_df["group"] == group]["risk"].values[0]
+        lesser_value = group_vals.iloc[:, 2].values
+        greater_value = group_vals.iloc[:, 3].values
+        base = baseline_vals[baseline_vals["group"] == group]["risk"].values[0]
 
-        # Draw bars for 75%
-        for y, value2 in zip(ys, value_75):
-            ax.broken_barh(
-                [(base, value2)], (y - height / 2, height), facecolors=[color_95, color_95]
-            )
-
-        # Draw bars for 25%
-        for y, value in zip(ys, value_25):
-            ax.broken_barh(
-                [(base, value)], (y - height / 2, height), facecolors=[color_5, color_5]
-            )
+        # Draw bars
+        for y, value, value2 in zip(ys, lesser_value, greater_value):
+            if abs(base - value) < abs(base - value2):
+                ax.broken_barh(
+                    [(base, value2 - base)],
+                    (y - height / 2, height),
+                    facecolors=[color_greater, color_greater],
+                )
+                ax.broken_barh(
+                    [(base, value - base)],
+                    (y - height / 2, height),
+                    facecolors=[color_lesser, color_lesser],
+                )
+            else:
+                ax.broken_barh(
+                    [(base, value - base)],
+                    (y - height / 2, height),
+                    facecolors=[color_lesser, color_lesser],
+                )
+                ax.broken_barh(
+                    [(base, value2 - base)],
+                    (y - height / 2, height),
+                    facecolors=[color_greater, color_greater],
+                )
 
         # Add vertical line for median value
         ax.axvline(base, color=color_line, linewidth=1.5, label="Median value")
 
         # Modify the graph
-        ax.set_ylim([-1.5, 5])
+        ax.set_ylim([-1.5, 6])
         ax.set_yticks(ys)
         ax.set_yticklabels(y_tick_label)
-        ax.set_xlabel(group, fontsize=12, fontweight="bold")
+        ax.set_title(group, fontsize=12, fontweight="bold")
 
-    # Add legend
-    handles = [
-        Patch(color=color_5, label="Bias at 25%"),
-        Patch(color=color_95, label="Bias at 75%"),
-        Line2D([0], [0], color=color_line, linewidth=1.5, label="Median value"),
+    return fig
+
+
+if __name__ == "__main__":
+    start_time = datetime.now()
+
+    # Define the argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--baseline")
+    parser.add_argument("--variable")
+    parser.add_argument("--baseline_sa")
+    parser.add_argument("--variable_sa")
+    parser.add_argument("--out_dir")
+    parser.add_argument("--num_samples", default=10000, type=int)
+    args = parser.parse_args()
+
+    baseline_dir = Path(args.baseline)
+    variable_dir = Path(args.variable)
+    baseline_dir_sa = Path(args.baseline_sa)
+    variable_dir_sa = Path(args.variable_sa)
+
+    out_dir = Path(args.out_dir)
+    num_samples = args.num_samples
+
+    param_df_baseline = pd.read_parquet(baseline_dir / "parameters.parquet").reset_index(drop=True)
+    param_df_variable = pd.read_parquet(variable_dir / "parameters.parquet").reset_index(drop=True)
+    param_df_baseline_sa = pd.read_parquet(baseline_dir_sa / "parameters.parquet").reset_index(
+        drop=True
+    )
+    param_df_variable_sa = pd.read_parquet(variable_dir_sa / "parameters.parquet").reset_index(
+        drop=True
+    )
+
+    # load the baseline data
+    bmi_int_dm_prev_baseline = dd.read_parquet(baseline_dir / "dm_final_output.parquet")
+    bmi_int_dm_prev_baseline_sa = dd.read_parquet(baseline_dir_sa / "dm_final_output.parquet")
+
+    # clean to control specifications
+    control_bmi_int_dm_prev_baseline = clean_control(
+        bmi_int_dm_prev_baseline, only_eligible=True, only_received=True
+    )
+    control_bmi_int_dm_prev_baseline_sa = clean_control(
+        bmi_int_dm_prev_baseline_sa, only_eligible=True, only_received=True
+    )
+
+    # filter for only people eligible for intervention
+    bmi_int_eligible_risk_baseline = calc_risk_by_group(
+        control_bmi_int_dm_prev_baseline, 7
+    ).compute()
+    bmi_int_eligible_risk_baseline_sa = calc_risk_by_group(
+        control_bmi_int_dm_prev_baseline_sa, 7
+    ).compute()
+
+    baseline_risk_median = (
+        bmi_int_eligible_risk_baseline.groupby("group")["risk"].median().reset_index()
+    )
+    baseline_risk_sa_median = (
+        bmi_int_eligible_risk_baseline_sa.groupby("group")["risk"].median().reset_index()
+    )
+
+    # load the variable data
+    bmi_int_dm_prev_variable = dd.read_parquet(variable_dir / "dm_final_output.parquet")
+    bmi_int_dm_prev_variable_sa = dd.read_parquet(variable_dir_sa / "dm_final_output.parquet")
+
+    # clean to control specifications
+    control_bmi_int_dm_prev_variable = clean_control(
+        bmi_int_dm_prev_variable, only_eligible=True, only_received=True
+    )
+    control_bmi_int_dm_prev_variable_sa = clean_control(
+        bmi_int_dm_prev_variable_sa, only_eligible=True, only_received=True
+    )
+
+    # filter for only people eligible for intervention
+    bmi_int_eligible_risk_variable = calc_risk_by_group(
+        control_bmi_int_dm_prev_variable, 7
+    ).compute()
+    bmi_int_eligible_risk_variable_sa = calc_risk_by_group(
+        control_bmi_int_dm_prev_variable_sa, 7
+    ).compute()
+
+    variable_risk_median = (
+        bmi_int_eligible_risk_variable.groupby("group")["risk"].median().reset_index()
+    )
+
+    variable_risk_sa_median = (
+        bmi_int_eligible_risk_variable_sa.groupby("group")["risk"].median().reset_index()
+    )
+
+    # gather samples from each non SA run
+    baseline_risk = (
+        bmi_int_eligible_risk_baseline.groupby("group")
+        .sample(num_samples, replace=True)
+        .reset_index()
+    )
+    variable_risk = (
+        bmi_int_eligible_risk_variable.groupby("group")
+        .sample(num_samples, replace=True)
+        .reset_index()
+    )
+
+    # take the difference between the samples
+    risk_df = pd.DataFrame(variable_risk["risk"] - baseline_risk["risk"])
+
+    # add back the group column that is lost
+    risk_df["group"] = baseline_risk["group"]
+
+    # take the median across groups
+    baseline_risk_difference_df = risk_df.groupby("group").median().reset_index()
+
+    # gather samples from each SA run
+    baseline_risk_sa = (
+        bmi_int_eligible_risk_baseline_sa.groupby("group")
+        .sample(num_samples, replace=True)
+        .reset_index()
+    )
+    variable_risk_sa = (
+        bmi_int_eligible_risk_variable_sa.groupby("group")
+        .sample(num_samples, replace=True)
+        .reset_index()
+    )
+
+    # take the difference between the samples
+    risk_df_sa = pd.DataFrame(variable_risk_sa["risk"] - baseline_risk_sa["risk"])
+
+    # add back the group column that is lost
+    risk_df_sa["group"] = baseline_risk_sa["group"]
+
+    # take the median across groups
+    baseline_risk_difference_df_sa = risk_df_sa.groupby("group").median().reset_index()
+
+    target_columns = [
+        "prev_users_dict_dm",
+        "prev_inits_dict_dm",
+        "sa_incidence_scalar_dm",
+        "pre_art_bmi",
+        "post_art_bmi",
+        "art_initiators",
     ]
-    plt.legend(handles=handles, loc="lower right")
 
-    fig.savefig(out_dir / "tornado.png")
+    variable_name_map = {
+        "prev_users_dict_dm": "DM Prevalence at 2009",
+        "prev_inits_dict_dm": "DM Prevalence 2009-end",
+        "sa_incidence_scalar_dm": "DM Incidence",
+        "pre_art_bmi": "Pre ART BMI",
+        "post_art_bmi": "Post ART BMI",
+        "art_initiators": "# ART Initiators",
+    }
+
+    baseline_tornado_vals = []
+    variable_tornado_vals = []
+    difference_tornado_vals = []
+    for col in target_columns:
+        baseline_vals, variable_vals, difference_vals = calc_tornado_vals(
+            bmi_int_eligible_risk_baseline_sa,
+            bmi_int_eligible_risk_variable_sa,
+            param_df_baseline_sa,
+            param_df_variable_sa,
+            col,
+            lesser=0.10,
+            greater=0.90,
+        )
+        baseline_tornado_vals.append(baseline_vals)
+        variable_tornado_vals.append(variable_vals)
+        difference_tornado_vals.append(difference_vals)
+    baseline_tornado_vals = pd.concat(baseline_tornado_vals).reset_index(drop=True)
+    variable_tornado_vals = pd.concat(variable_tornado_vals).reset_index(drop=True)
+    difference_tornado_vals = pd.concat(difference_tornado_vals).reset_index(drop=True)
+
+    # rename variable to semantic labels
+    baseline_tornado_vals["variable"] = baseline_tornado_vals["variable"].map(variable_name_map)
+    variable_tornado_vals["variable"] = variable_tornado_vals["variable"].map(variable_name_map)
+    difference_tornado_vals["variable"] = difference_tornado_vals["variable"].map(
+        variable_name_map
+    )
+
+    baseline_tornado = tornado_plot(baseline_tornado_vals, baseline_risk_sa_median)
+
+    baseline_tornado.savefig(out_dir / "baseline_tornado.png")
+    plt.clf()
+
+    variable_tornado = tornado_plot(variable_tornado_vals, variable_risk_sa_median)
+
+    variable_tornado.savefig(out_dir / "variable_tornado.png")
+    plt.clf()
+
+    difference_tornado = tornado_plot(difference_tornado_vals, baseline_risk_difference_df_sa)
+
+    difference_tornado.savefig(out_dir / "tornado.png")
+    plt.clf()
